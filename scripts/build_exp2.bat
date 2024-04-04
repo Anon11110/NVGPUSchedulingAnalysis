b3b601949a94c5a8c6ebc1126ccf098bfb86d0e4
Anon <ym-li@outlook.com>
2024-04-04 10:28:56 -0400

    Implemented Exp4 for testing shared memory competition

--- scripts/build_exp2.bat
+++ scripts/build_exp2.bat
@@ -5,7 +5,8 @@ set SRCFILES=../src/Experiment2.cu ../src/KernelsExp1.cu ../src/TimerSpin.cu
 set OUTPUT=-o Experiment2.exe
 set INCLUDES=-I "../include"
 set DEBUGFLAGS=-G -lineinfo -Xptxas -v
+set RELEASEFLAGS=-lineinfo -Xptxas -v -O3
 
-nvcc -ccbin %VSCOMPILER% %SRCFILES% %OUTPUT% %DEBUGFLAGS% %INCLUDES% %LIBPATHS% %ARCH_FLAGS% -lcudart -std=c++17
+nvcc -ccbin %VSCOMPILER% %SRCFILES% %OUTPUT% %RELEASEFLAGS% %INCLUDES% %LIBPATHS% %ARCH_FLAGS% -lcudart -std=c++17
 
 echo Compilation complete.
